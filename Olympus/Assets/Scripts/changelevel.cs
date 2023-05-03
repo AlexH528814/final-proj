45e12ee063f200ec101fdb9cdf1fcdf5219febe5
Alex Huddle <528814@escg.ac.uk>
2023-05-03 16:02:15 +0100

    dgut

---
+++ Olympus/Assets/Scripts/changelevel.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class changelevel : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("player"))
+        {
+
+            string scene = SceneManager.GetActiveScene().name;
+            switch (scene)
+            {
+                case "Level1":
+                    Debug.Log("changed");
+                    SceneManager.LoadScene("Level2");
+                    break;
+
+                case "Level2":
+                    SceneManager.LoadScene("Level3");
+                    break;
+
+                case "Level3":
+                    SceneManager.LoadScene("End");
+                    break;
+            }
+        } 
+    }
+}
